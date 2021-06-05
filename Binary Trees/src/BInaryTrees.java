e03841714ccd4da6793faed4c1f41a5f62d4c2ba
vivek <vivekumar175@gmail.com>
2021-06-05 11:52:36 +0530

    Diameter added too

--- Binary Trees/src/BInaryTrees.java
+++ Binary Trees/src/BInaryTrees.java
@@ -37,9 +37,22 @@ public class BInaryTrees {
 		}
 	}
 	
+	static int diameter(Node root) {
+		if(root ==null)
+			return 0;
+		int lheight=height(root.left);
+		int rheight=height(root.right);
+		
+		int ldiameter=diameter(root.left);
+		int rdiameter=diameter(root.right);
+		
+		return Math.max(lheight+rheight+1 ,Math.max(ldiameter, rdiameter));
+	}
+	
 	static void pgl(Node root,int i) {
 		if(root==null) return ;
 		if(i==1) System.out.print(root.data + " ");
+		
 		else if(i>1){
 			pgl(root.left,i-1);
 			pgl(root.right,i-1);
@@ -65,6 +78,8 @@ public class BInaryTrees {
 	    System.out.println();
 	    System.out.println("Height of binary tree is");
 	    System.out.println(height(root));
+	    System.out.println("Diameter of binary tree is ");
+	    System.out.println(diameter(root));
 	}
 
 }
