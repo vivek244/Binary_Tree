32d3c8731883127b0f754ada7b0b1a2ba9d1067b
vivek <vivekumar175@gmail.com>
2021-06-07 21:03:12 +0530

    updated

--- Binary Trees/src/LCA.java
+++ Binary Trees/src/LCA.java
@@ -1,59 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
 
-class Node {
-    int data;
-    Node left, right;
- 
-    Node(int value) {
-        data = value;
-        left = right = null;
-    }
-}
+//class Node {
+//    int data;
+//    Node left, right;
+// 
+//    Node(int value) {
+//        data = value;
+//        left = right = null;
+//    }
+//}
 
 public class LCA {
-	static Node node;
-	private static List<Integer> path1=new ArrayList<>();
-	private static List<Integer> path2=new ArrayList<>();
 	
-	static int find(int n1,int n2) {
-		path1.clear();
-		path2.clear();
-		return Lca(node,n1,n2);
-	}
+	public static class Node {
+        int value;
+        Node left;
+        Node right;
+ 
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 	
-	public static int Lca(Node node,int n1,int n2) {
-		 if (!findPath(node, n1, path1) || !findPath(node, n2, path2)) {
-	            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
-	            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
-	            return -1;
-	        }
-	 
-	        int i;
-	        for (i = 0; i < path1.size() && i < path2.size(); i++) {
-	             
-	            if (!path1.get(i).equals(path2.get(i)))
-	                break;
-	        }
-	 
-	        return path1.get(i-1);
-	}
 	
-	public static boolean findPath(Node node,int n,List<Integer> path) {
-		if(node ==null) return false;
-		path.add(node.data);
-		if(node.data==n) return true;
-		
-		if((node.left!=null) && findPath(node.left, n, path))
-			return true;
-		
-		if((node.right!=null) && findPath(node.right, n, path))
-			return true;
-		
-		path.remove(path.size()-1);
-		
-		return false;
-	}
+	public static Node LCA(Node root, int n1, int n2)
+    {
+        if (root == null)
+            return root;
+        if (root.value == n1 || root.value == n2)
+            return root;
+ 
+        Node left = LCA(root.left, n1, n2);
+        Node right = LCA(root.right, n1, n2);
+ 
+        if (left != null && right != null)
+            return root;
+          if (left == null && right == null)
+              return null;
+        if (left != null)
+            return LCA(root.left, n1, n2);
+        else
+            return LCA(root.right, n1, n2);
+    }
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -64,8 +52,11 @@ public class LCA {
 		node.left.right=new Node(5);
 		node.right.left =new Node(6);
 		node.right.right =new Node(7);
-		System.out.println(find(4,5));
+		Node lca=LCA(node,3,4);
+		System.out.println(lca.value);
 
 	}
 
 }
+
+
